3b697400d06f65c77590f6bc94e43f09b4b3e774
Mwile Isaac Mulemfwe <isaacmulemfwe@gmail.com>
2026-02-27 20:15:58 +0000

    File changes

--- src/pages/ShopDashboard.jsx
+++ src/pages/ShopDashboard.jsx
@@ -25,6 +25,7 @@ import { Switch } from "@/components/ui/switch";
 import { toast } from "sonner";
 import DashboardSidebar from "@/components/dashboard/DashboardSidebar";
 import ShopPartsRequests from "@/components/parts/ShopPartsRequests";
+import MarketInsights from "@/components/analytics/MarketInsights";
 import StatsCard from "@/components/analytics/StatsCard";
 import SalesChart from "@/components/analytics/SalesChart";
 import CategoryChart from "@/components/analytics/CategoryChart";
